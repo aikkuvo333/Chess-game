2fa1dbc27ab71c2173bae703726ef6fb23b710cf
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-02-01 16:14:57 +0200

    Kuninkaan testejä parannettu

--- src/test/java/model/KuningasTest.java
+++ src/test/java/model/KuningasTest.java
@@ -27,7 +27,7 @@ class KuningasTest {
 		assertTrue("Valkoinen Kuningas ei palauttanut oikeata variä",
 				((Kuningas) lauta.getLauta()[4][5].getNappula()).getVari() == NappulanVari.MUSTA);
 	}
-	
+
 	@Test
 	@DisplayName("getEkaSiirto metodin testaus")
 	public void getEkaSiirto() {
@@ -36,36 +36,39 @@ class KuningasTest {
 		assertFalse("Valkoinen Kuningas ei palauttanut false",
 				((Kuningas) lauta.getLauta()[4][5].getNappula()).getEkaSiirto());
 		lauta.siirra(4, 5, 5, 5);
-		assertTrue("Valkoinen Kuiningas ei palauttanut true", ((Kuningas) lauta.getLauta()[5][5].getNappula()).getEkaSiirto());
+		assertTrue("Valkoinen Kuiningas ei palauttanut true",
+				((Kuningas) lauta.getLauta()[5][5].getNappula()).getEkaSiirto());
 
 		lauta = new Lauta();
 		lauta.getLauta()[4][5].setNappula(new Kuningas(NappulanVari.MUSTA));
-		assertFalse("Musta Kuiningas ei palauttanut false", ((Kuningas) lauta.getLauta()[4][5].getNappula()).getEkaSiirto());
+		assertFalse("Musta Kuiningas ei palauttanut false",
+				((Kuningas) lauta.getLauta()[4][5].getNappula()).getEkaSiirto());
 		lauta.siirra(4, 5, 5, 5);
-		assertTrue("Musta Kuiningas ei palauttanut true", ((Kuningas) lauta.getLauta()[5][5].getNappula()).getEkaSiirto());
+		assertTrue("Musta Kuiningas ei palauttanut true",
+				((Kuningas) lauta.getLauta()[5][5].getNappula()).getEkaSiirto());
 	}
-	
+
 	@Test
 	@DisplayName("Kuningas ei palauta yli rajojen meneviä siirtoja")
-	public void testaaTorninRajat() {
+	public void testaaRajatKuninkaalla() {
 		lauta = new Lauta(true);
 		lauta.getLauta()[0][0].setNappula(new Kuningas(NappulanVari.VALKOINEN));
 		ArrayList<Ruutu> siirrot = lauta.getSiirrot(0, 0);
 		assertFalse("Siirto laudan ulkopuolella vasemmassa alanurkassa", menikoYli(siirrot));
 		assertEquals(3, siirrot.size(), "Vasen alanurkka palautti vääränä määrän siirtoja");
-		
+
 		lauta = new Lauta(true);
 		lauta.getLauta()[0][7].setNappula(new Kuningas(NappulanVari.VALKOINEN));
 		siirrot = lauta.getSiirrot(0, 7);
 		assertFalse("Siirto laudan ulkopuolella vasemmassa ylänurkassa", menikoYli(siirrot));
 		assertEquals(3, siirrot.size(), "Vasen ylänurkka palautti vääränä määrän siirtoja");
-		
+
 		lauta = new Lauta(true);
 		lauta.getLauta()[7][7].setNappula(new Kuningas(NappulanVari.VALKOINEN));
 		siirrot = lauta.getSiirrot(7, 7);
 		assertFalse("Siirto laudan ulkopuolella oikeassa ylänurkassa", menikoYli(siirrot));
 		assertEquals(3, siirrot.size(), "Oikea ylänurkka palautti vääränä määrän siirtoja");
-		
+
 		lauta = new Lauta(true);
 		lauta.getLauta()[7][0].setNappula(new Kuningas(NappulanVari.VALKOINEN));
 		siirrot = lauta.getSiirrot(7, 0);
@@ -73,7 +76,72 @@ class KuningasTest {
 		assertEquals(3, siirrot.size(), "Oikea alanurkka palautti vääränä määrän siirtoja");
 	}
 
-	
+	@Test
+	@DisplayName("Palauttaa oikean määrän siirtoja omien ympäröimänä")
+	public void omienYmparoimana() {
+
+		// Testataan valkoinen
+		lauta = new Lauta(true);
+		lauta.getLauta()[4][4].setNappula(new Kuningas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[5][4].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[5][5].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[4][5].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[3][5].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[3][4].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[3][3].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[4][3].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[5][3].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		ArrayList<Ruutu> siirrot = lauta.getSiirrot(4, 4);
+		assertEquals(0, siirrot.size(), "Valkoinen Kuningas palautti liikaa siirtoja");
+
+		// Testataan musta
+		lauta = new Lauta(true);
+		lauta.getLauta()[4][4].setNappula(new Kuningas(NappulanVari.MUSTA));
+		lauta.getLauta()[5][4].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[5][5].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[4][5].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[3][5].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[3][4].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[3][3].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[4][3].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[5][3].setNappula(new Sotilas(NappulanVari.MUSTA));
+		siirrot = lauta.getSiirrot(4, 4);
+		assertEquals(0, siirrot.size(), "Musta Kuningas palautti liikaa siirtoja");
+	}
+
+	@Test
+	@DisplayName("Palauttaa oikean määrän siirtoja vihollisten ympäröimänä")
+	public void vihollistenYmparoimana() {
+
+		// Testataan valkoinen Kuningas
+		lauta = new Lauta(true);
+		lauta.getLauta()[4][4].setNappula(new Kuningas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[5][4].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[5][5].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[4][5].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[3][5].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[3][4].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[3][3].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[4][3].setNappula(new Sotilas(NappulanVari.MUSTA));
+		lauta.getLauta()[5][3].setNappula(new Sotilas(NappulanVari.MUSTA));
+		ArrayList<Ruutu>  siirrot = lauta.getSiirrot(4, 4);
+		assertEquals(8, siirrot.size(), "Valkoinen Kuningas palautti liikaa siirtoja");
+
+		// Testataan musta Kuningas
+		lauta = new Lauta(true);
+		lauta.getLauta()[4][4].setNappula(new Kuningas(NappulanVari.MUSTA));
+		lauta.getLauta()[5][4].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[5][5].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[4][5].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[3][5].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[3][4].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[3][3].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[4][3].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		lauta.getLauta()[5][3].setNappula(new Sotilas(NappulanVari.VALKOINEN));
+		siirrot = lauta.getSiirrot(4, 4);
+		assertEquals(8, siirrot.size(), "Musta Kuningas palautti liikaa siirtoja");
+	}
+
 	private boolean menikoYli(ArrayList<Ruutu> siirrot) {
 		for (Ruutu ruutu : siirrot) {
 			if (ruutu.getX() > 7 || ruutu.getX() < 0 || ruutu.getY() > 7 || ruutu.getY() < 0) {
