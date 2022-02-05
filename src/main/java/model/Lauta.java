e365f574081c16fb013f93ba07f9f22800ffdff5
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-02-05 11:12:24 +0200

    Listätty getRuutu metodi. Helpottaa testejä.

--- src/main/java/model/Lauta.java
+++ src/main/java/model/Lauta.java
@@ -6,9 +6,18 @@ import java.util.ArrayList;
  * @author Elmo Vahvaselkä 26.1.2022
  */
 
+/* Ei otaa kantaa sääntöihin. 
+ * Huolehtii, että nappulat ovat siellä, 
+ * minne Shakkipeliluokka ne haluaa laittaa.
+ * Helpottaa testejä, kun laudalla voi 
+ * tehdä mitä huvittaa.
+ * */
+ 
 public class Lauta {
 
 	private Ruutu[][] lauta;
+	private Ruutu valkoinenKuningas;
+	private Ruutu mustaKuningas;
 
 	public Lauta() {
 		this.lauta = new Ruutu[8][8];
@@ -25,6 +34,10 @@ public class Lauta {
 	public Ruutu[][] getLauta() {
 		return this.lauta;
 	}
+	
+	public Ruutu getRuutu(int x, int y) {
+		return this.lauta[x][y];
+	}
 
 	public ArrayList<Ruutu> getSiirrot(int x, int y) {
 		return lauta[x][y].getNappula().getSiirrot(new Ruutu(x, y), this.lauta);
@@ -33,8 +46,35 @@ public class Lauta {
 	public boolean siirra(int mistaX, int mistaY, int mihinX, int mihinY) {
 		Nappula nappula = lauta[mistaX][mistaY].poistaNappula();
 		lauta[mihinX][mihinY].setNappula(nappula);
+		if (nappula instanceof Kuningas) {
+			paivitaKuninkaanSijainti((Kuningas)nappula, new Ruutu(mihinX, mihinY));
+		}
 		merkitseEkaSiirto(nappula);
 		return true;
+	}
+	
+	public Ruutu getValkoinenKuningas() {
+		return this.valkoinenKuningas;
+	}
+	
+	public Ruutu getMustaKuningas() {
+		return this.mustaKuningas;
+	}
+	
+	private void paivitaKuninkaanSijainti(Kuningas kuningas, Ruutu ruutu) {
+		if (kuningas.getVari() == NappulanVari.VALKOINEN) {
+			this.setValkoinenKuningas(ruutu);
+		} else {
+			this.setMustaKuningas(ruutu);
+		}
+	}
+	
+	private void setValkoinenKuningas(Ruutu ruutu) {
+		this.valkoinenKuningas = ruutu;
+	}
+	
+	private void setMustaKuningas(Ruutu ruutu) {
+		this.mustaKuningas = ruutu;
 	}
 
 	private void asetaNappulat() {
@@ -42,6 +82,15 @@ public class Lauta {
 			lauta[x][1].setNappula(new Sotilas(NappulanVari.VALKOINEN));
 			lauta[x][6].setNappula(new Sotilas(NappulanVari.MUSTA));
 		}
+		
+		lauta[4][0].setNappula(new Kuningas(NappulanVari.VALKOINEN));
+		this.setValkoinenKuningas(new Ruutu(4,0));
+		lauta[3][7].setNappula(new Kuningas(NappulanVari.MUSTA));
+		this.setMustaKuningas(new Ruutu(3,7));
+		lauta[0][0].setNappula(new Torni(NappulanVari.VALKOINEN));
+		lauta[7][0].setNappula(new Torni(NappulanVari.VALKOINEN));
+		lauta[0][7].setNappula(new Torni(NappulanVari.MUSTA));
+		lauta[7][7].setNappula(new Torni(NappulanVari.MUSTA));
 	}
 
 	private void luoRuudut() {
@@ -64,4 +113,5 @@ public class Lauta {
 		}
 	}
 
+	
 }
