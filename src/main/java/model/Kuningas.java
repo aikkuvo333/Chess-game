939a67789d03d6537aa3bfe068fa8e6798ba4c33
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-02-08 14:00:13 +0200

    Kuningas palauttaa mahdolliset tornitukset siirtoina

--- src/main/java/model/Kuningas.java
+++ src/main/java/model/Kuningas.java
@@ -68,6 +68,42 @@ public class Kuningas extends Nappula {
 				|| lauta[ruutu.getX() + 1][ruutu.getY()].getNappula().getVari() != this.vari)) {
 			siirrot.add(new Ruutu(ruutu.getX() + 1, ruutu.getY()));
 		}
+		
+		if(this.vari == NappulanVari.VALKOINEN) {
+			//Valkoisen Kuninkaan tornitus oikealle
+			if(!this.ekaSiirto && lauta[7][0].getNappula() instanceof Torni) {
+				Torni torni = (Torni)lauta[7][0].getNappula();
+				if(torni.getEkaSiirto() == false && lauta[6][0].getNappula() == null && lauta[5][0].getNappula() == null) {
+					siirrot.add(new Ruutu(6,0));
+				}
+			}
+			
+			//Tornitus vasemmalle
+			if(!this.ekaSiirto && lauta[0][0].getNappula() instanceof Torni) {
+				Torni torni = (Torni)lauta[0][0].getNappula();
+				if(torni.getEkaSiirto() == false && lauta[1][0].getNappula() == null && lauta[2][0].getNappula() == null && lauta[3][0].getNappula() == null) {
+					siirrot.add(new Ruutu(2,0));
+				}
+			}
+		}
+		
+		if(this.vari == NappulanVari.MUSTA) {
+			//Valkoisen Kuninkaan tornitus oikealle
+			if(!this.ekaSiirto && lauta[7][7].getNappula() instanceof Torni) {
+				Torni torni = (Torni)lauta[7][7].getNappula();
+				if(torni.getEkaSiirto() == false && lauta[6][7].getNappula() == null && lauta[5][7].getNappula() == null) {
+					siirrot.add(new Ruutu(6,7));
+				}
+			}
+			
+			//Tornitus vasemmalle
+			if(!this.ekaSiirto && lauta[0][7].getNappula() instanceof Torni) {
+				Torni torni = (Torni)lauta[0][7].getNappula();
+				if(torni.getEkaSiirto() == false && lauta[1][7].getNappula() == null && lauta[2][7].getNappula() == null && lauta[3][7].getNappula() == null) {
+					siirrot.add(new Ruutu(2,7));
+				}
+			}
+		}
 
 		return siirrot;
 	}
