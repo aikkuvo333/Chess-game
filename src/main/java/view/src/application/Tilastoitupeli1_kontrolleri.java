8bf10d19a55fbfb72d8688d413f4f29ff78b70d2
vanv <aivanvo@aivan-mbp.home>
2022-02-17 13:52:26 +0200

    Tilastoidun pelin pelaajavalintaikkuna luotu + sille kontrolleri.

---
+++ src/main/java/view/src/application/Tilastoitupeli1_kontrolleri.java
@@ -0,0 +1,72 @@
+package view.src.application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
+
+/*
+ * @author Aivan Vo 3.2.2022
+ */
+
+public class Tilastoitupeli1_kontrolleri {
+
+	@FXML
+    private Button poistuBtn;
+
+    @FXML
+    private MenuButton valkVetovalikkoBtn;
+
+    @FXML
+    private TextField valkTekstikenttaBtn;
+
+    @FXML
+    private Button valkLuotunnusBtn;
+
+    @FXML
+    private Button aloitaPeliBtn;
+
+    @FXML
+    private TextField mustVetovalikkoBtn;
+
+    @FXML
+    private Button mustLuotunnusBtn;
+
+    @FXML
+    void aloitaPeli(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mustLuotunnus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mustVetovalikko(ActionEvent event) {
+
+    }
+
+    @FXML
+    void poistu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void valkLuotunnus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void valkTekstikentta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void valkVetovalikko(ActionEvent event) {
+
+    }
+
+}
