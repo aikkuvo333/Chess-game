c80a5584b2dd7d0e5e090978c976700453374a37
vanv <aivanvo@localhost>
2022-03-08 12:20:47 +0200

    poistu nappien toiminnon lisäys

--- src/main/java/view/src/application/Tilastot2_kontrolleri.java
+++ src/main/java/view/src/application/Tilastot2_kontrolleri.java
@@ -1,12 +1,19 @@
 package view.src.application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /*
  * @author Aivan Vo 27.2.2022
@@ -14,6 +21,10 @@ import javafx.scene.text.Text;
 
 public class Tilastot2_kontrolleri {
 
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
     @FXML
     private MenuButton pelaajaMenuBtn;
 
@@ -59,8 +70,12 @@ public class Tilastot2_kontrolleri {
     }
 
     @FXML
-    void poistu(ActionEvent event) {
-
+    void poistu(ActionEvent event) throws IOException {
+    	root = FXMLLoader.load(getClass().getResource("Alkuvalikko.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
