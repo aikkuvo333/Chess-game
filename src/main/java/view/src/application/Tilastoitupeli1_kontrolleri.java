4b6056dbcc5b3619b280e8644bc4525bcf5ea517
vanv <aivanvo@aivan-mbp.home>
2022-03-10 16:23:05 +0200

    poistu napissa onglemaa joten laitettu kommentteihin.

--- src/main/java/view/src/application/Tilastoitupeli1_kontrolleri.java
+++ src/main/java/view/src/application/Tilastoitupeli1_kontrolleri.java
@@ -1,10 +1,17 @@
 package view.src.application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 /*
@@ -12,7 +19,10 @@ import javafx.scene.control.TextField;
  */
 
 public class Tilastoitupeli1_kontrolleri {
-
+//	private Stage stage;
+//	private Scene scene;
+//	private Parent root;
+	
     @FXML
     private Button poistuBtn;
 
@@ -39,7 +49,7 @@ public class Tilastoitupeli1_kontrolleri {
 
     @FXML
     void aloitaPeli(ActionEvent event) {
-
+    	System.out.println("klick");
     }
 
     @FXML
@@ -58,8 +68,13 @@ public class Tilastoitupeli1_kontrolleri {
     }
 
     @FXML
-    void poistu(ActionEvent event) {
-
+    void poistu(ActionEvent event) throws IOException {
+    	System.out.println("klikattu");
+//    	root = FXMLLoader.load(getClass().getResource("Alkuvalikko.fxml"));
+//        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+//        scene = new Scene(root);
+//        stage.setScene(scene);
+//        stage.show();
     }
 
     @FXML
