cf591fd3ea8ba0124632403237725d0bb5532b9f
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-04-11 06:42:25 +0300

    Alkuvalinkon kontrolleria päivitetty

--- src/main/java/view/src/application/Alkuvalikko_kontrolleri.java
+++ src/main/java/view/src/application/Alkuvalikko_kontrolleri.java
@@ -61,7 +61,9 @@ public class Alkuvalikko_kontrolleri {
 
 	@FXML
 	void TilastoituPeli(ActionEvent event) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("Tilastoitupeli1.fxml"));
+		loader = new FXMLLoader(getClass().getResource("Tilastoitupeli1.fxml"));
+		loader.setResources(ValittuKieli.getInstance().getBundle());
+		root = loader.load();
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
