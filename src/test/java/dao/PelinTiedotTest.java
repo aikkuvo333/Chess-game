1014751862884843dd0a461f3c523ad2b007bf45
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-04-15 15:08:00 +0300

    Lisätty PelinTiedot luokan testikattavuutta

--- src/test/java/dao/PelinTiedotTest.java
+++ src/test/java/dao/PelinTiedotTest.java
@@ -1,8 +1,10 @@
 package dao;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -35,5 +37,41 @@ class PelinTiedotTest {
 		assertTrue("Valkoinen sotilas ei liikkunut", peli.siirra(0, 1, 0, 2));
 		peli.julistaVoittaja();
 		assertEquals("Valkoinen", peli.getPelinTiedot().getVoittaja().getKayttajaTunnus(), "Voittaja tellentui väärin");
+	}
+	
+	@Test
+	@DisplayName("Siirtoja tallentui oikea määrä")
+	public void siirtojenMaaraOikein() {
+		assertTrue("Valkoinen sotilas ei siirtynyt 1", peli.siirra(5, 1, 5, 2));
+		assertTrue("Musta sotilas ei siirtynyt", peli.siirra(4, 6, 4, 5));
+		assertTrue("Valkoinen sotilas ei siirtynyt 2", peli.siirra(6, 1, 6, 3));
+		assertTrue("Musta Kuningatar ei siirtynyt ja lopettanut peliä", peli.siirra(3, 7, 7, 3));
+		assertEquals(4, peli.getPelinTiedot().getSiirrot().size(), "siirtoja tallentui väärä määrä");
+	}
+	
+	@Test
+	@DisplayName("Päivämäärä tallentui oikein")
+	public void paivamaaranTallentuminen() {
+		assertTrue("Valkoinen sotilas ei siirtynyt 1", peli.siirra(5, 1, 5, 2));
+		assertTrue("Musta sotilas ei siirtynyt", peli.siirra(4, 6, 4, 5));
+		assertTrue("Valkoinen sotilas ei siirtynyt 2", peli.siirra(6, 1, 6, 3));
+		assertTrue("Musta Kuningatar ei siirtynyt ja lopettanut peliä", peli.siirra(3, 7, 7, 3));
+		
+		SimpleDateFormat mutoilija = new SimpleDateFormat("dd-MM-yyyy");
+		Date paivamaara = new Date();
+		
+		assertEquals(mutoilija.format(paivamaara), mutoilija.format(peli.getPelinTiedot().getPvm()), "Päivämäärä tallentui väärin");
+	}
+	
+	@Test
+	@DisplayName("Kesto talleentuu suunnilleen oikein")
+	public void kestonTallentuminen() throws InterruptedException{
+		Thread.sleep(1000);
+		assertTrue("Valkoinen sotilas ei siirtynyt 1", peli.siirra(5, 1, 5, 2));
+		assertTrue("Musta sotilas ei siirtynyt", peli.siirra(4, 6, 4, 5));
+		assertTrue("Valkoinen sotilas ei siirtynyt 2", peli.siirra(6, 1, 6, 3));
+		assertTrue("Musta Kuningatar ei siirtynyt ja lopettanut peliä", peli.siirra(3, 7, 7, 3));
+		assertTrue("Peli ei kestänyt yli sekunttia", peli.getPelinTiedot().getKesto() > 0);
+		assertTrue("Peli kesti yli 2 sekunttia", peli.getPelinTiedot().getKesto() < 2);
 	}
 }
