7a819b6d5040ab426c37ee380d4daaa043a6ef69
vanv <aivanvo@localhost>
2022-04-15 23:37:59 +0300

    Ohjenappula luotu. nappula vie ohje url:ään

--- src/main/java/view/src/application/Alkuvalikko_kontrolleri.java
+++ src/main/java/view/src/application/Alkuvalikko_kontrolleri.java
@@ -1,5 +1,6 @@
 package view.src.application;
 
+import java.awt.Desktop;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -10,6 +11,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /*
  * @author Aivan Vo 5.2.2022
@@ -36,24 +39,27 @@ public class Alkuvalikko_kontrolleri {
 
 	@FXML
 	private Button leaderboardBtn;
-	
+
 	@FXML
-    private Button finnish;
-	
+	private Button finnish;
+
 	@FXML
-    private Button english;
-	
+	private Button english;
+
+	@FXML
+	private Button instructions;
+
 	@FXML
 	void TilastoimatonPeli(ActionEvent event) throws IOException {
 
 		PeliNakyma controller = new PeliNakyma();
-		//Asetukset_kontrolleri asetukset_kontrolleri = new Asetukset_kontrolleri();
+		// Asetukset_kontrolleri asetukset_kontrolleri = new Asetukset_kontrolleri();
 		loader = new FXMLLoader(getClass().getResource("Lauta.fxml"));
 		loader.setResources(ValittuKieli.getInstance().getBundle());
 		loader.setController(controller);
 		root = loader.load();
-		
-		stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
@@ -64,10 +70,10 @@ public class Alkuvalikko_kontrolleri {
 		loader = new FXMLLoader(getClass().getResource("Tilastoitupeli1.fxml"));
 		loader.setResources(ValittuKieli.getInstance().getBundle());
 		root = loader.load();
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 
 	@FXML
@@ -75,16 +81,16 @@ public class Alkuvalikko_kontrolleri {
 		loader = new FXMLLoader(getClass().getResource("Leaderboard.fxml"));
 		loader.setResources(ValittuKieli.getInstance().getBundle());
 		root = loader.load();
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 
 	@FXML
 	void suljeSovellus(ActionEvent event) {
 		stage = (Stage) suljeSobellusBtn.getScene().getWindow();
-	    stage.close();
+		stage.close();
 	}
 
 	@FXML
@@ -92,38 +98,46 @@ public class Alkuvalikko_kontrolleri {
 		loader = new FXMLLoader(getClass().getResource("Tilastot2.fxml"));
 		loader.setResources(ValittuKieli.getInstance().getBundle());
 		root = loader.load();
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
-	
+
 	@FXML
-	void changeLanguageToFin (ActionEvent event) throws IOException {
+	void changeLanguageToFin(ActionEvent event) throws IOException {
 		System.out.println("change to FIN");
 		ValittuKieli.getInstance().setSuomi();
-		
+
 		loader = new FXMLLoader(getClass().getResource("Alkuvalikko.fxml"));
 		loader.setResources(ValittuKieli.getInstance().getBundle());
 		root = loader.load();
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
-	
+
 	@FXML
-	void changeLanguageToUs (ActionEvent event) throws IOException {
+	void changeLanguageToUs(ActionEvent event) throws IOException {
 		System.out.println("change to US");
 		ValittuKieli.getInstance().setEnglanti();
-		
+
 		loader = new FXMLLoader(getClass().getResource("Alkuvalikko.fxml"));
 		loader.setResources(ValittuKieli.getInstance().getBundle());
 		root = loader.load();
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	@FXML
+	void openInstructions(ActionEvent event) throws IOException, URISyntaxException {
+		System.out.println("ohjetta klikattu");
+		
+		Desktop d = Desktop.getDesktop();
+		d.browse(new URI("http://www.shakkimatti.net/saannot.php"));
 	}
 
 }
