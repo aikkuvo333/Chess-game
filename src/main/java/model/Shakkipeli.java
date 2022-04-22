02c71c743a92fe6320202024de04d636774a462d
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-04-22 21:35:55 +0300

    Shakkipelille javadoc, testikonstruktorista poistettu turha parametri

--- src/main/java/model/Shakkipeli.java
+++ src/main/java/model/Shakkipeli.java
@@ -2,10 +2,6 @@ package model;
 
 import java.util.ArrayList;
 
-/**
-* @author Elmo Vahvaselkä 27.1.2022
-*/
-
 import controller.IKontrolleri;
 import dao.DBKontrolleri;
 import dao.IDaoController;
@@ -13,19 +9,68 @@ import dao.Pelaaja;
 import dao.PelinTiedot;
 import dao.Siirto;
 
-//Luokka huolehtii säännösitä
+/**
+ * Shakkipeli luokka huolehtii Shakkipelin säännöistä.
+ * @author Elmo Vahvaselkä 27.1.2022
+ *
+ */
 public class Shakkipeli implements IShakkipeli {
+	
+	/**
+	 * MVC-mallin mukainen kontrolleri, jonka avulla Shakkipeli kommunikoi viewvin kanssa.
+	 */
 	private IKontrolleri kontrolleri;
+	
+	/**
+	 * Lauta olio, joka kuvaa shakin pelilautaa ja sisältää tiedot nappuloiden sijainneista.
+	 */
 	private Lauta lauta;
+	
+	/**
+	 * Vuorossa olevan nappulan väri enuminen, jonka arvo voi olla VALKOINEN tai MUSTA.
+	 */
 	private NappulanVari vuorossa;
+	
+	/**
+	 * Ilmaisee onko peli shakatussa tilassa.
+	 */
 	private boolean shakattu;
+	
+	/**
+	 * Boolean joka asetettaan trueksi, kun halutaan suorittaa junit-testejä.
+	 */
 	private boolean testi;
+	
+	/**
+	 * Arvo, joka asetetaan trueksi, kun halutaan pelata tilastoitu peli, jonka tiedot tallennetaan tietokantaan.
+	 */
 	private boolean tilastoitu;
+	
+	/**
+	 * Ilmaisee onko peli loppunut.
+	 */
 	private boolean peliLoppunut;
+	
+	/**
+	 * PelinTiedot olio, jota käytetään pelin tietojen tallentamiseen.
+	 */
 	private PelinTiedot pelinTiedot;
+	
+	/**
+	 * Enum arvo, jolla voidaan määrittää junit testeissä tehtävät sotilaan korotukset.
+	 */
 	private NappulanTyyppi testiKorotus;
+	
+	/**
+	 * Tietokannan kontrolleri, jota käytetään pelin tietojen tietokantaan tallentamiseen.
+	 */
 	private IDaoController daoKontrolleri;
 
+	/**
+	 * Konstruktori Shakkipelille.
+	 * @param kontrolleri MVC-mallin mukainen kontrolleri, jonka avulla Shakkipeli kommunikoi viewvin kanssa. 
+	 * @param tilastoitu True mikäli kyseessä tilastoitu peli muuten false.
+	 */
 	public Shakkipeli(IKontrolleri kontrolleri, boolean tilastoitu) {
 		this.kontrolleri = kontrolleri;
 		this.lauta = new Lauta();
@@ -43,8 +88,10 @@ public class Shakkipeli implements IShakkipeli {
 		}
 	}
 
-	// Konstruktori testeille
-	public Shakkipeli(boolean tilastoitu) {
+	/**
+	 * Kostruktori testeille.
+	 */
+	public Shakkipeli() {
 		this.lauta = new Lauta();
 		this.vuorossa = NappulanVari.VALKOINEN;
 		this.shakattu = false;
@@ -56,10 +103,16 @@ public class Shakkipeli implements IShakkipeli {
 		this.pelinTiedot = new PelinTiedot(new Pelaaja("Valkoinen"), new Pelaaja("Musta"));
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public Ruutu[][] getPelitilanne() {
 		return this.lauta.getLauta();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public boolean siirra(int mistaX, int mistaY, int mihinX, int mihinY) {
 		if (siirtyykoOikeaVari(mistaX, mistaY) && onkoSiirtoListalla(mistaX, mistaY, mihinX, mihinY)
 				&& !this.peliLoppunut) {
@@ -92,11 +145,11 @@ public class Shakkipeli implements IShakkipeli {
 		return false;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public ArrayList<Ruutu> getSiirrot(int x, int y) {
 		ArrayList<Ruutu> siirrot = this.lauta.getSiirrot(x, y);
-
-		// Jos aikaa jää täydennä tänne siirroon sopivuuksien tarkistus, jotta pelilauta
-		// näyttää vain täysin lailliset siirrot
 
 		// Tornitusta ei tarjota mikäli shakattu
 		if (getRuudunNappula(x, y).getTyyppi() == NappulanTyyppi.KUNINGAS && shakattu) {
@@ -120,7 +173,78 @@ public class Shakkipeli implements IShakkipeli {
 		return siirrot;
 
 	}
+	
+	/**
+	 * Metodia kutsumalla peli päättyy. Ilmoittaa voittajan viewille ja tallentaa pelin tietokantaan,
+	 * mikäli kyseessä oli tilastoitu peli.
+	 */
+	public void julistaVoittaja() {
+		this.peliLoppunut = true;
+		if (this.vuorossa == NappulanVari.VALKOINEN) {
+			pelinTiedot.setVoittaja(pelinTiedot.getMustaPelaaja());
+		} else {
+			pelinTiedot.setVoittaja(pelinTiedot.getValkoinenPelaaja());
+		}
 
+		if (tilastoitu) {
+			daoKontrolleri.tallennaPeli(pelinTiedot);
+		}
+
+		if (!this.testi) {
+			this.kontrolleri.pelinvoitti(pelinTiedot.getVoittaja());
+		}
+	}
+	
+	/**
+	 * Palauttaa tiedon siitä, onko peli shakattu.
+	 * @return Palauttaa true, mikäli peli on shakattu, muuten false;
+	 */
+	public boolean getShakattu() {
+		return this.shakattu;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public NappulanVari getVuoro() {
+		return vuorossa;
+	}
+
+	/**
+	 * Metodilla voi varmistaa onko peli loppunut();
+	 * @return Palauttaa true, mikäli peli on loppunut, muuten false.
+	 */
+	public boolean getPeliLoppunut() {
+		return this.peliLoppunut;
+	}
+
+	/**
+	 * Metodilla voi hakea pelin voittaneen pelaajan.
+	 * @return palauttaa pelin voittajan Pelaaja oliona.
+	 */
+	public Pelaaja getVoittaja() {
+		return this.pelinTiedot.getVoittaja();
+	}
+	
+	/**
+	 * Metodilla voi hakea pelin tiedot.
+	 * @return PelinTiedot olion.
+	 */
+	public PelinTiedot getPelinTiedot() {
+		return pelinTiedot;
+	}
+
+	/**
+	 * Metodilla voi asettaa korotettavan NappulanTyypin testejä varten.
+	 * @param tyyppi
+	 */
+	public void setTestiKorotus(NappulanTyyppi tyyppi) {
+		this.testiKorotus = tyyppi;
+	}
+	
+	/**
+	 * Vaihtaa vuorossa olevan pelaajan.
+	 */
 	private void vaihdaVuoro() {
 		if (this.vuorossa == NappulanVari.VALKOINEN) {
 			this.vuorossa = NappulanVari.MUSTA;
@@ -129,10 +253,24 @@ public class Shakkipeli implements IShakkipeli {
 		}
 	}
 
+	/**
+	 * Metodin avulla voidaan varmistaa, että siirretään vuorssa olevan värin mukaista nappulaa.
+	 * @param x Siirrettävän nappulan x-koordinatti, joka on kokonaisluku väliltä 1-7.
+	 * @param y Siirrettävän nappulan x-koordinatti, joka on kokonaisluku väliltä 1-7.
+	 * @return boolean arvo true, mikäli siirretään oikeata väriä, muuten false.
+	 */
 	private boolean siirtyykoOikeaVari(int x, int y) {
 		return this.getRuudunNappula(x, y).getVari() == this.vuorossa;
 	}
 
+	/**
+	 * Tarkastaa että onko suoritettava siirto sallittujen siirtojen listalla.
+	 * @param mistaX Siirrettävän pelinappulan lähtökoordinaatti x-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 * @param mistaY Siirrettävän pelinappulan lähtökoordinaatti y-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 * @param mihinX Siirrettävän pelinappulan kohdekoordinaatti x-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 * @param mihinY Siirrettävän pelinappulan kohdekoordinaatti y-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 * @return Palauttaa true, mikäli siirto on sallittu, muuten false.
+	 */
 	private boolean onkoSiirtoListalla(int mistaX, int mistaY, int mihinX, int mihinY) {
 		ArrayList<Ruutu> siirrot = this.getSiirrot(mistaX, mistaY);
 		for (Ruutu siirto : siirrot) {
@@ -143,10 +281,11 @@ public class Shakkipeli implements IShakkipeli {
 		return false;
 	}
 
-	public boolean getShakattu() {
-		return this.shakattu;
-	}
-
+	/**
+	 * Tarkaastaa aiheuttaako tiettyyn ruutuun siirrety nappula shakin.
+	 * @param x Nappulan sijainti x-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 * @param y Nappulan sijainti y-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 */
 	private void tarkistaShakkasiko(int x, int y) {
 
 		// Haetaan siirretyn nappulan mahdolliset seuraavat siirrot
@@ -167,26 +306,14 @@ public class Shakkipeli implements IShakkipeli {
 		}
 	}
 
-	private boolean vaarantuukoKuningas(int mihinX, int mihinY) {
-		for (int y = 0; y < 8; y++) {
-			for (int x = 0; x < 8; x++) {
-				if (this.getRuudunNappula(x, y) instanceof Nappula) {
-					if (this.getRuudunNappula(x, y).getVari() != vuorossa) {
-						ArrayList<Ruutu> siirrot = this.getRuudunNappula(x, y).getSiirrot(new Ruutu(x, y),
-								this.getPelitilanne());
-
-						for (Ruutu siirto : siirrot) {
-							if (siirto.getX() == mihinX && siirto.getY() == mihinY) {
-								return true;
-							}
-						}
-					}
-				}
-			}
-		}
-		return false;
-	}
-
+	/**
+	 * Varmistaa, että tehtävä siirto on turvallinen. Mikäli siirto ei ole turvallinen peli pysyy 
+	 * shakatussa tilassa ja käyttäjän siirtoa ei hyväksytä siirrä() metodissa.
+	 * @param mistaX Siirrettävän pelinappulan lähtökoordinaatti x-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 * @param mistaY Siirrettävän pelinappulan lähtökoordinaatti y-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 * @param mihinX Siirrettävän pelinappulan kohdekoordinaatti x-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 * @param mihinY Siirrettävän pelinappulan kohdekoordinaatti y-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 */
 	private void varmistaSiirronTurvallisuus(int mistaX, int mistaY, int mihinX, int mihinY) {
 		Nappula nappula = null;
 		Boolean ekaSiirtoTehty = true;
@@ -229,7 +356,37 @@ public class Shakkipeli implements IShakkipeli {
 			this.lauta.getRuutu(mihinX, mihinY).setNappula(nappula);
 		}
 	}
+	
+	/**
+	 * Tarkistaa vaarantuuko kuningas siirron seuruksena. Apumetodi varmistaSiirronTurvallisuus() metodille.
+	 * @param x Nappulan sijainti x-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 * @param y Nappulan sijainti y-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 * @return Boolean arvo true, mikäli kuningas vaarantuu, muuten false.
+	 */
+	private boolean vaarantuukoKuningas(int mihinX, int mihinY) {
+		for (int y = 0; y < 8; y++) {
+			for (int x = 0; x < 8; x++) {
+				if (this.getRuudunNappula(x, y) instanceof Nappula) {
+					if (this.getRuudunNappula(x, y).getVari() != vuorossa) {
+						ArrayList<Ruutu> siirrot = this.getRuudunNappula(x, y).getSiirrot(new Ruutu(x, y),
+								this.getPelitilanne());
 
+						for (Ruutu siirto : siirrot) {
+							if (siirto.getX() == mihinX && siirto.getY() == mihinY) {
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Metodia käytetään tarkistamaan, että päättyikö peli.
+	 * @return Palauttaa true, mikäli peli päättyi, muuten false.
+	 */
 	private boolean paattyikoPeli() {
 
 		// Peli ei voi päättyä mikäli Kuningasta ei ole shakattu
@@ -262,23 +419,8 @@ public class Shakkipeli implements IShakkipeli {
 		return true;
 	}
 
-	public void julistaVoittaja() {
-		this.peliLoppunut = true;
-		if (this.vuorossa == NappulanVari.VALKOINEN) {
-			pelinTiedot.setVoittaja(pelinTiedot.getMustaPelaaja());
-		} else {
-			pelinTiedot.setVoittaja(pelinTiedot.getValkoinenPelaaja());
-		}
-
-		if (tilastoitu) {
-			daoKontrolleri.tallennaPeli(pelinTiedot);
-		}
-
-		if (!this.testi) {
-			this.kontrolleri.pelinvoitti(pelinTiedot.getVoittaja());
-		}
-	}
-
+	
+	
 	private NappulanTyyppi teeMahdollinenKorotus(int x, int y) {
 		NappulanTyyppi tyyppi = null;
 		if (y == 7 && getRuudunNappula(x, y).getTyyppi() == NappulanTyyppi.SOTILAS) {
@@ -301,29 +443,15 @@ public class Shakkipeli implements IShakkipeli {
 		}
 		return tyyppi;
 	}
-
-	public NappulanVari getVuoro() {
-		return vuorossa;
-	}
-
-	public boolean getPeliLoppunut() {
-		return this.peliLoppunut;
-	}
-
-	public Pelaaja getVoittaja() {
-		return this.pelinTiedot.getVoittaja();
-	}
-
+	
+	/**
+	 * Metodilla voi hakea tietyssä ruudussa olevan Nappula olion
+	 * @param x Nappulan sijainti x-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 * @param y Nappulan sijainti y-akselilla, jonka arvon on kokonaisluku väliltä 0-7.
+	 * @return Palauttaa Nappula olion.
+	 */
 	private Nappula getRuudunNappula(int x, int y) {
 		return lauta.getRuutu(x, y).getNappula();
-	}
-
-	public PelinTiedot getPelinTiedot() {
-		return pelinTiedot;
-	}
-
-	public void setTestiKorotus(NappulanTyyppi tyyppi) {
-		this.testiKorotus = tyyppi;
 	}
 
 }
