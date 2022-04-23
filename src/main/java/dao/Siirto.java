f3ad79136767aecc97e74de0c8b7e4d4f96b9038
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-04-23 20:01:47 +0300

    Javadoc luotu Siirto luokalle

--- src/main/java/dao/Siirto.java
+++ src/main/java/dao/Siirto.java
@@ -16,33 +16,59 @@ import javax.persistence.Table;
 
 import model.NappulanTyyppi;
 
+/**
+ * Tähän luokkaan talletetaan yksittäiseen siirtoon liittyvät tiedot.
+ * @author Elmo Vahvaselkä
+ *
+ */
 
 @Entity
 @Table
 public class Siirto {
-	@Id
-	@Column
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	/**
+	 * Siirron id integerinä.
+	 */
+	@Id @Column @GeneratedValue(strategy = GenerationType.IDENTITY) private int id;
 	
-	@Column
-	private int mistaX;
+	/**
+	 * Siirron lähtökoordinaatti x-akselilla.
+	 */
+	@Column private int mistaX;
 	
-	@Column
-	private int mistaY;
+	/**
+	 * Siirron lähtökoordinaatti y-akselilla.
+	 */
+	@Column private int mistaY;
 	
-	@Column
-	private int mihinX;
+	/**
+	 * Siirron kohdekoordinaatti x-akselilla.
+	 */
+	@Column private int mihinX;
 	
-	@Column
-	private int mihinY;
+	/**
+	 * Siirron kohdekoordinaatti y-akselilla.
+	 */
+	@Column private int mihinY;
 	
-	@Column
-	@Enumerated(EnumType.STRING)
-	private NappulanTyyppi korotus;
+	/**
+	 * Mahdollinen korotus NappulanTyyppi enumina.
+	 */
+	@Column @Enumerated(EnumType.STRING) private NappulanTyyppi korotus;
 
+	/**
+	 * Parametriton kostruktori.
+	 */
 	public Siirto() {}
 	
+	
+	/**
+	 * Konstruktori, jolla luodaan uusi siirto.
+	 * @param mistaX Siirron lähtökoordinaatti x-akselilla.
+	 * @param mistaY Siirron lähtökoordinaatti y-akselilla.
+	 * @param mihinX Siirron kohdekoordinaatti x-akselilla.
+	 * @param mihinY Siirron kohdekoordinaatti y-akselilla.
+	 * @param korotus Kortotus NappulanTyyppi enumina.
+	 */
 	public Siirto(int mistaX, int mistaY, int mihinX, int mihinY, NappulanTyyppi korotus) {
 		this.mistaX = mistaX;
 		this.mistaY = mistaY;
@@ -51,26 +77,42 @@ public class Siirto {
 		this.korotus = korotus;
 	}
 
+	/**
+	 * Getteri lähtöruudun x-koordinaatille.
+	 * @return x-koordinaatti integerinä.
+	 */
 	public int getMistaX() {
 		return mistaX;
 	}
 
-
+	/**
+	 * Getteri lähtöruudun y-koordinaatille.
+	 * @return y-koordinaatti integerinä.
+	 */
 	public int getMistaY() {
 		return mistaY;
 	}
 
-
+	/**
+	 * Getteri kohderuudun x-koordinaatille.
+	 * @return x-koordinaatti integerinä.
+	 */
 	public int getMihinX() {
 		return mihinX;
 	}
 
-
+	/**
+	 * Getteri kohderuudun y-koordinaatille.
+	 * @return y-koordinaatti integerinä.
+	 */
 	public int getMihinY() {
 		return mihinY;
 	}
 
-
+	/**
+	 * Getteri kortukselle.
+	 * @return Korotus NappulanTyyppi enumina.
+	 */
 	public NappulanTyyppi getKorotus() {
 		return korotus;
 	}
