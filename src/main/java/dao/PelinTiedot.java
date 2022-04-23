feca0658b9c6b3ca7aa0a66a51a5cc8f2004092d
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-04-23 19:51:39 +0300

    PelinTiedot javadoc luotu

--- src/main/java/dao/PelinTiedot.java
+++ src/main/java/dao/PelinTiedot.java
@@ -1,9 +1,5 @@
 package dao;
 
-/*
- * @author Oliver Hamberg, Elmo vahvaselkä
- */
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -20,32 +16,65 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+/**
+ * PelinTiedot on luokkaan tallennetaan tiedot pelin kulusta.
+ * Luokkaa käytetään myös apuna tietojen tallettamiseen ja 
+ * lukemiseen tietokannasta.
+ * @author Oliver Hamberg
+ * @author Elmo Vahvaselkä
+ *
+ */
 @Entity
 @Table
 public class PelinTiedot {
-	@Id
-	@Column
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	/**
+	 * Pelin tiedot identifioiva luku.
+	 */
+	@Id @Column @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int peliId;
-	@ManyToOne
-	private Pelaaja valkoinenPelaaja;
-	@ManyToOne
-	private Pelaaja mustaPelaaja;
-	@ManyToOne
-	private Pelaaja voittaja;
+	
+	/**
+	 * Valkoinen pelaaja Pelaaja oliona.
+	 */
+	@ManyToOne private Pelaaja valkoinenPelaaja;
+	
+	/**
+	 * Musta pelaaja Pelaaja oliona.
+	 */
+	@ManyToOne private Pelaaja mustaPelaaja;
+	
+	/**
+	 * Pelin voittaja Pelaaja oliona.
+	 */
+	@ManyToOne private Pelaaja voittaja;
 
-	@Column
-	@Temporal(TemporalType.DATE)
-	private Date pvm;
-	@Column
-	private long kesto;
+	/**
+	 * Päivämäärä jolloin peli on pelattu Date oliona.
+	 */
+	@Column @Temporal(TemporalType.DATE) private Date pvm;
+	
+	/**
+	 * Pelin kesto sekuntteina.
+	 */
+	@Column private long kesto;
 
-	@OneToMany(cascade = CascadeType.ALL)
-	private List<Siirto> siirrot;
+	/**
+	 * Lista olio, johon kirjataan pelissä tehdyt siirrot Siirto olioina.
+	 */
+	@OneToMany(cascade = CascadeType.ALL) private List<Siirto> siirrot;
 
+	/**
+	 * Parametriton konstruktori.
+	 */
 	public PelinTiedot() {
 	}
 
+	/**
+	 * Konstruktori, jota Shakkipeli luokka kutsuu, kun peli aloitetaan.
+	 * Kirjaa talteen myös alkamisajan luonnin yhteydessä.
+	 * @param valkoinenPelaaja Valkoinen pelaaja Pelaaja oliona.
+	 * @param mustaPelaaja Musta pelaaja Pelaaja oliona.
+	 */
 	public PelinTiedot(Pelaaja valkoinenPelaaja, Pelaaja mustaPelaaja) {
 		this.valkoinenPelaaja = valkoinenPelaaja;
 		this.mustaPelaaja = mustaPelaaja;
@@ -53,40 +82,79 @@ public class PelinTiedot {
 		pvm = new Date();
 	}
 
+	/**
+	 * Tallettaa pelissä tehdyn siirron.
+	 * @param siirto tehty siirto Siirto oliona.
+	 */
 	public void lisaaSiirto(Siirto siirto) {
 		this.siirrot.add(siirto);
 	}
 
+	/**
+	 * Palauttaa pelin voittajan.
+	 * @return Palauttaa pelin voittajan Pelaaja oliona.
+	 */
 	public Pelaaja getVoittaja() {
 		return voittaja;
 	}
 
+	/**
+	 * Metodia kutsutaan, kun halutaan asettaa pelin
+	 * voittaja pelin päätteenksi. Laskee myös peliin 
+	 * kuluneen ajan sekuntteina.
+	 * @param voittaja Pelin voittaja Pelaaja oliona.
+	 */
 	public void setVoittaja(Pelaaja voittaja) {
 		// lasketaan peliin kulunut aika sekunteina
 		kesto = ((new Date().getTime()) - pvm.getTime()) / (1000);
 		this.voittaja = voittaja;
 	}
 
+	/**
+	 * Getteri valkoiselle pelaajalle.
+	 * @return Palauttaa valkoisen pelaajan Pelaaja oliona.
+	 */
 	public Pelaaja getValkoinenPelaaja() {
 		return valkoinenPelaaja;
 	}
 
+	/**
+	 * Getteri mustalle pelaajalle.
+	 * @return Palauttaa mustan pelaajan Pelaaja oliona.
+	 */
 	public Pelaaja getMustaPelaaja() {
 		return mustaPelaaja;
 	}
 
+	/**
+	 * Palauttaa pelissä tehdyt siirrot.
+	 * @return Pelissä tehdyt siirrot Siirto olioina Listissä.
+	 */
 	public List<Siirto> getSiirrot() {
 		return this.siirrot;
 	}
 
+	/**
+	 * Palautta pelin päivämäärän.
+	 * @return Päivämäärä Date oliona.
+	 */
 	public Date getPvm() {
 		return pvm;
 	}
 
+	/**
+	 * Palautaaa pelin pituuden sekuntteina.
+	 * @return Pelin kesto sekuntteina long integerinä.
+	 */
 	public long getKesto() {
 		return kesto;
 	}
 	
+	
+	/**
+	 * Getteri pelin id:lle.
+	 * @return palauttaa pelin id:en integerinä.
+	 */
 	public int getId() {
 		return peliId;
 	}
