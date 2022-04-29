05f6ef3f2344bfc829a37539fc195df98b978313
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-04-29 20:58:24 +0300

    Modelin javadoc luotu.

---
+++ src/main/java/model/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Sisältää shakkipelin toiminnallisuuteen liittyvät luokat.
+ * <p>
+ * MVC-mallin mukainen model pakkaus, jossa tarvittava toiminnallisuus shakkipelille.
+ * </p>
+ * @author Elmo Vahvaselkä 29.2.2022
+ */
+package model;
