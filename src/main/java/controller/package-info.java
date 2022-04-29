c71f6988f300a769482f470e0198053048e03ab1
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-04-29 20:57:46 +0300

    Controller pakkauksen javadoc luotu.

---
+++ src/main/java/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Sisältää MVC-mallin mukaisen kontrollerin toiminallisuuden.
+ * <p>
+ * Kontrolleri toimii välikätenä model ja view pakkauksien välillä.
+ * <p>
+ * @author Elmo Vahvaselkä 29.4.2022
+ */
+package controller;
