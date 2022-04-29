3a4520587a95e1a7b32e0a196d5ed5ab68a7c768
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-04-29 20:58:10 +0300

    Dao pakkauksen javadoc luotu.

---
+++ src/main/java/dao/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Sisältää pelitietojen tallentamiseen liittyvän toiminnallisuuden.
+ * <p>
+ * Tietokanta toteutettu Hibernate ORM:illa. 
+ * </p>
+ * @author Elmo Vahvaselkä 29.4.2022
+*/
+package dao;
