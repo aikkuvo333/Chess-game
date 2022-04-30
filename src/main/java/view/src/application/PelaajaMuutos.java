6ec2cf2dd9f4be9d3f65d272010fb8d6f08b86e7
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-04-30 20:02:19 +0300

    Javadoc PelaajaMuutos luokalle.

--- src/main/java/view/src/application/PelaajaMuutos.java
+++ src/main/java/view/src/application/PelaajaMuutos.java
@@ -3,15 +3,37 @@ package view.src.application;
 import dao.Pelaaja;
 
 /**
+ * Luokka jota käytetään muuttamaan Pelaaja-luokan tiedot
+ * JavaFX:än TableViewille sopivaan muotoon.
  * @author Elmo Vahvaselä
  */
-
 public class PelaajaMuutos {
+	/**
+	 * Pelaajan tunnus String-oliona.
+	 */
 	private String kayttajaTunnus;
+	
+	/**
+	 * Pelaajan voittojen määrä String-oliona.
+	 */
 	private String voitot;
+	
+	/**
+	 * Pelaajan voittot String-oliona. Decimaalierotin esitetään localen mukaan.
+	 */
 	private String voittoprosentti;
+	
+	/**
+	 * Pelatut pelit Strgin-oliona.
+	 */
 	private String peleja;
 
+	/**
+	 * Konstruktori, joka saa parametrinä Pelaaja-oliona sen pelaajan,
+	 * jonka tiedot halutaan muuttaa. Kaikki Pelaaja-olion tiedot muutetaan
+	 * String-olioiksi ja voittoprosentti muokataan localen mukaiseksi.
+	 * @param pelaaja
+	 */
 	public PelaajaMuutos(Pelaaja pelaaja) {
 		kayttajaTunnus = pelaaja.getKayttajaTunnus();
 		voitot = pelaaja.getVoitot() + "";
@@ -19,18 +41,34 @@ public class PelaajaMuutos {
 		peleja = pelaaja.getPeleja() + "";
 	}
 
+	/**
+	 * Getteri käyttäjätunnukselle.
+	 * @return Käyttäjätunnus String-oliona.
+	 */
 	public String getKayttajaTunnus() {
 		return kayttajaTunnus;
 	}
 
+	/**
+	 * Getteri voittojen määrälle.
+	 * @return Voittojen määrä String-oliona.
+	 */
 	public String getVoitot() {
 		return voitot;
 	}
 
+	/**
+	 * Getteri voittoprosentille.
+	 * @return Voittoprosentti String-oliona, joka on muotoiltu localen mukaan.
+	 */
 	public String getVoittoprosentti() {
 		return voittoprosentti;
 	}
 
+	/**
+	 * Getteri pelattujen pelien määrälle.
+	 * @return Pelattujen pelien määrä String-oliona.
+	 */
 	public String getPeleja() {
 		return peleja;
 	}
