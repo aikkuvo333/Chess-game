7068afe94a10150ac37a95e7c3acb03f2f1a0052
Elmo V. <72126010+ElmoVah@users.noreply.github.com>
2022-04-30 20:19:15 +0300

    JavaDoc PeliMuutos luokalle.

--- src/main/java/view/src/application/PeliMuutos.java
+++ src/main/java/view/src/application/PeliMuutos.java
@@ -6,17 +6,65 @@ import java.util.ResourceBundle;
 import dao.Pelaaja;
 import dao.PelinTiedot;
 
+/**
+ * Luokka jota käytetään muuttamaan PelinTiedot-luokan tiedot
+ * JavaFX:än TableViewille sopivaan muotoon. PelinTiedot-luokasta
+ * poiketen, PeliMuutos-luokan tiedot tietyn pelaajan näkökulmasta.
+ * Tarkoittaen, että vari, vastustaja ja tulos päätellään konstruktorin
+ * parametrien mukaan.
+ * @author Elmo Vahvaselkä
+ *
+ */
 public class PeliMuutos {
+	/**
+	 * Sen pelaajan vari, joka annetaan parametrinä konstruktorissa.
+	 */
 	private String vari;
+	
+	/**
+	 * Konstruktorissa annetun pelaajan vastustaja.
+	 */
 	private String vastustaja;
+	
+	/**
+	 * Voittiko vai hävisikö konstruktorissa olevan pelaajan pelin. 
+	 * Localen mukaan talletetaan joko sana voitto tai häviö.
+	 */
 	private String tulos;
+	
+	/**
+	 * Pelissä tehtyjen siirtojen määrä String-oliona.
+	 */
 	private String siirrot;
+	
+	/**
+	 * Päivämäärä jolloin pelin on pelattu String-oliona. Muotoiltu localen mukaan.
+	 */
 	private String pvm;
+	
+	/**
+	 * Pelin kesto String-oliona. Ilmoitetaan tunteina, minuutteina ja sekunteina. 
+	 * Yksiköt lokalisoitu. Nollaa olevia yksiköitä ei näytetä. 
+	 */
 	private String kesto;
+	
+	/**
+	 * DateFormat-olio, jolla mutoillaan päivämäärä localen mukaan sopivaan muotoon.
+	 */
 	private DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT,
 			ValittuKieli.getInstance().getLocale());
+	
+	/**
+	 * Localen mukaiset tekstiresurssit.
+	 */
 	private ResourceBundle bundle = ValittuKieli.getInstance().getBundle();
 
+	/**
+	 * Konstruktori joka päättelee pelaajan värin ja tuloksen.
+	 * Muokkaa String-oliot sopiviksi localen mukaan.
+	 * @param PelinTiedot-olio siitä pelistä, joka halutaan muuttaa.
+	 * @param Pelaaja-olio, jonka näkökulmasta peliä halutaan tarkastella.
+	 */
 	public PeliMuutos(PelinTiedot pelinTiedot, Pelaaja pelaaja) {
 		if (pelinTiedot.getValkoinenPelaaja() == pelaaja) {
 			vari = bundle.getString("PeliMuutosValkoinenTxt");
@@ -45,26 +93,50 @@ public class PeliMuutos {
 
 	}
 
+	/**
+	 * Getteri värille.
+	 * @return Pelaajan väri String-oliona
+	 */
 	public String getVari() {
 		return vari;
 	}
 
+	/**
+	 * Getteri vastustajalle.
+	 * @return Pelaajan vastusta String-oliona.
+	 */
 	public String getVastustaja() {
 		return vastustaja;
 	}
 
+	/**
+	 * Getteri tulokselle.
+	 * @return Pelin tulos String-oliona localen mukaisella kielellä.
+	 */
 	public String getTulos() {
 		return tulos;
 	}
 
+	/**
+	 * Getterin siirtojen määrälle.
+	 * @return Pelissä tehtyjen siirtojen lukumäärä String-oliona.
+	 */
 	public String getSiirrot() {
 		return siirrot;
 	}
 
+	/**
+	 * Getteri päivämäärälle.
+	 * @return Pelin päivämäärä String-oliona localen mukaan muotoiltuna.
+	 */
 	public String getPvm() {
 		return pvm;
 	}
 
+	/**
+	 * Getteri pelin kestolle.
+	 * @return Pelin kesto String-oliona localen ja pelin pituuden mukaan muotoiltuna.
+	 */
 	public String getKesto() {
 		return kesto;
 	}
