915a453ed0b9a6b8f9b17c772c314c93fe4f21e8
vanv <aivanvo@aivan-mbp.home>
2022-05-02 23:32:13 +0300

    toimimaton dark mode poistettu koodista ja näkymästä + javadoc

--- src/main/java/view/src/application/Asetukset.java
+++ src/main/java/view/src/application/Asetukset.java
@@ -7,49 +7,80 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
 
-/*
+/**
+ * 
+ * Luokka joka sisältää käyttöliittymän pelaamiseen liittyvät pelilliset
+ * asetukset
  * 
  * @author Santeri Kuusisto
  * 
  */
 public class Asetukset {
-	private static final String CONFIG_FILE = "config.properties";
+
+	/**
+	 * boolean arvo laudan animaatiolle
+	 */
 	private boolean laudanAnimaatio;
-	private boolean darkMode;
+
+	/**
+	 * boolean arvo peliäänille
+	 */
 	private boolean aanet;
 
+	/**
+	 * Konstruktori, joka kutsuu metodia joka hakee tekstitiedostosta
+	 * oletusasetukset.
+	 */
 	public Asetukset() {
 		haeAsetukset();
 	}
 
+	/**
+	 * Metodi joka asettaa laudan animaation päälle tai pois.
+	 * 
+	 * @param laudanAnimaatio boolean arvo, joka ollessaan true tarkoittaa että
+	 *                        laudan animaatio on päällä.
+	 */
 	public void setLaudanAnimaatio(boolean laudanAnimaatio) {
 		this.laudanAnimaatio = laudanAnimaatio;
 		System.out.println("Asetukset setLaudanAnimaatio(): " + this.laudanAnimaatio);
 	}
 
-	public void setDarkMode(boolean darkMode) {
-		this.darkMode = darkMode;
-		System.out.println("Asetukset setDarkMode(): " + this.darkMode);
-	}
-
+	/**
+	 * Metodi joka asettaa äänet päälle tai pois.
+	 * 
+	 * @param aanet boolean arvo, joka ollessaan true tarkoittaa että äänet ovat
+	 *              päällä.
+	 */
 	public void setAanet(boolean aanet) {
 		this.aanet = aanet;
 		System.out.println("Asetukset setAanet(): " + this.aanet);
 	}
 
+	/**
+	 * Metodi joka palauttaa boolean arvon, joka kertoo onko laudan animaatio päällä
+	 * vai ei
+	 * 
+	 * @return true jos laudan animaatio on päällä
+	 */
 	public boolean isLaudanAnimaatio() {
 		return laudanAnimaatio;
 	}
 
-	public boolean isDarkMode() {
-		return darkMode;
-	}
-
+	/**
+	 * Metodi joka palauttaa boolean arvon, joka kertoo onko äänet päällä vai ei
+	 * 
+	 * @return true jos äänet on päällä
+	 */
 	public boolean isAanet() {
 		return aanet;
 	}
 
-	// Asettaa oletusarvot, jos properties-tiedostoa ei löydy
+	/**
+	 * Asettaa oletusarvot, jos properties-tiedostoa ei löydy
+	 * 
+	 * @throws IOException osoittaa tietojen lukemisen aikana tapahtuvan virheen
+	 */
 	public static void initConfig() throws IOException {
 		Properties properties = new Properties();
 
@@ -57,7 +88,6 @@ public class Asetukset {
 			ip.close();
 		} catch (FileNotFoundException e) {
 			properties.setProperty("laudanAnimaatio", "true");
-			properties.setProperty("darkMode", "false");
 			properties.setProperty("aanet", "true");
 			FileWriter writer = new FileWriter("config.properties");
 			properties.store(writer, "Laudan asetukset");
@@ -65,13 +95,15 @@ public class Asetukset {
 		}
 	}
 
+	/**
+	 * Hakee tekstitiedosta tiedot asetuksia varten
+	 */
 	private void haeAsetukset() {
 		Properties properties = new Properties();
 		try (FileInputStream ip = new FileInputStream("config.properties")) {
 			properties.load(ip);
 
 			laudanAnimaatio = Boolean.parseBoolean(properties.getProperty("laudanAnimaatio"));
-			darkMode = Boolean.parseBoolean(properties.getProperty("darkMode"));
 			aanet = Boolean.parseBoolean(properties.getProperty("aanet"));
 
 			ip.close();
@@ -80,12 +112,14 @@ public class Asetukset {
 		}
 	}
 
+	/**
+	 * Asettaa tekstitiedosta haetut asetuksen tiedot
+	 */
 	public void asetaAsetukset() {
 		Properties properties = new Properties();
 		try (FileOutputStream op = new FileOutputStream("config.properties")) {
 
 			properties.setProperty("laudanAnimaatio", String.valueOf(laudanAnimaatio));
-			properties.setProperty("darkMode", String.valueOf(darkMode));
 			properties.setProperty("aanet", String.valueOf(aanet));
 			properties.store(op, null);
 
