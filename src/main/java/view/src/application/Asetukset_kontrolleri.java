fbf2adafe15d18d1fec4c642f0d2e2b68e13a743
vanv <aivanvo@aivan-mbp.home>
2022-05-02 23:41:47 +0300

    Asetukset kontrollerille javadoc

--- src/main/java/view/src/application/Asetukset_kontrolleri.java
+++ src/main/java/view/src/application/Asetukset_kontrolleri.java
@@ -7,31 +7,54 @@ import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 
 /**
+ * Luokka joka ohjaa kontrolloi Asetukset luokan oliota
  * @author Santeri Kuusisto
  */
 public class Asetukset_kontrolleri {
+	/**
+	 * Asetukset olio pelilaudan asetuksille
+	 */
 	private Asetukset asetukset;
+	/**
+	 * PeliNakyma olio pelilaudan kontrollerille
+	 */
 	private PeliNakyma lauta_kontrolleri;
+	/**
+	 * boolean arvo laudan animaatiolle
+	 */
 	private boolean asetus;
 
+	/**
+	 * Konstruktori, joka asettaa valitut asetukset Asetukset oliosta haluttuun
+	 * PeliNäkymä olioon
+	 * 
+	 * @param lauta_kontrolleri PeliNakymä olio johon asetukset kohdistetaan
+	 * @param asetukset         Asetukset olio josta haetaan asetusten tiedot
+	 *                          pelilautaa varten
+	 */
 	public Asetukset_kontrolleri(PeliNakyma lauta_kontrolleri, Asetukset asetukset) {
 		this.lauta_kontrolleri = lauta_kontrolleri;
 		this.asetukset = asetukset;
 	}
 
-	public Asetukset_kontrolleri() {
-
-	}
-
+	/**
+	 * RadioButton olio animaation sallimiseksi tai estämiseksi
+	 */
 	@FXML
 	private RadioButton animaatioAsetus;
 
-	@FXML
-	private RadioButton darkModeAsetus;
-
+	/**
+	 * RadioButton olio äänten sallimiseksi tai estämiseksi
+	 */
 	@FXML
 	private RadioButton aanetAsetus;
 
+	/**
+	 * animaatioAsetus- RadioButtonin metodi, jolla sallitaan tai kielletään laudan
+	 * animaatio
+	 * 
+	 * @param event animaatioAsetus- RadioButtonin napsautus
+	 */
 	@FXML
 	void asetaAnimaatio(ActionEvent event) {
 		asetus = animaatioAsetus.isSelected();
@@ -40,14 +63,11 @@ public class Asetukset_kontrolleri {
 		lauta_kontrolleri.asetaKaantyminen(asetus);
 	}
 
-	@FXML
-	void asetaDarkMode(ActionEvent event) {
-		asetus = darkModeAsetus.isSelected();
-		asetukset.setDarkMode(asetus);
-		asetukset.asetaAsetukset();
-		lauta_kontrolleri.asetaDarkMode(asetus);
-	}
-
+	/**
+	 * aanetAsetus- RadioButtonin metodi, jolla sallitaan tai kielletään peliäänet
+	 * 
+	 * @param event aanetAsetus- RadioButtonin napsautus
+	 */
 	@FXML
 	void asetaAanet(ActionEvent event) {
 		asetus = aanetAsetus.isSelected();
@@ -56,10 +76,14 @@ public class Asetukset_kontrolleri {
 		lauta_kontrolleri.asetaAanet(asetus);
 	}
 
+	/**
+	 * Oletusasetusten hakeminen ja näiden implementointi käyttöliittymään
+	 * 
+	 * @throws IOException osoittaa tietojen lukemisen aikana tapahtuvan virheen
+	 */
 	public void initialize() throws IOException {
 		Asetukset.initConfig();
 		animaatioAsetus.setSelected(asetukset.isLaudanAnimaatio());
-		darkModeAsetus.setSelected(asetukset.isDarkMode());
 		aanetAsetus.setSelected(asetukset.isAanet());
 	}
 }
