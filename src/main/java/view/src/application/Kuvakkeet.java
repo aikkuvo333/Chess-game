d7ea6a86591e8948735065d76c8010307a710c3f
vanv <aivanvo@aivan-mbp.home>
2022-05-02 22:26:34 +0300

    kuvakkeet luokan javadoc

--- src/main/java/view/src/application/Kuvakkeet.java
+++ src/main/java/view/src/application/Kuvakkeet.java
@@ -4,33 +4,85 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import model.NappulanVari;
 
+/**
+ * Luokka joka sisältää pelilaudalla olevien pelinappuloiden kuvat
+ * 
+ * @author Santeri Kuusisto
+ *
+ */
 public class Kuvakkeet {
+	/**
+	 * Kansio josta kuvat haetaan
+	 */
 	private static String kuvaURI = "File:src/main/resources/images/";
 
+	/**
+	 * Metodi joka palauttaa Sotilas -pelinappulan halutussa värissä
+	 * 
+	 * @param vari pelinappulan väri
+	 * @return ImageView olio kuvasta halutussa värissä
+	 */
 	public static ImageView getSotilas(NappulanVari vari) {
 		return new ImageView(new Image(kuvaURI + "sotilas" + getVariURI(vari)));
 	}
 
+	/**
+	 * Metodi joka palauttaa Hevonen -pelinappulan halutussa värissä
+	 * 
+	 * @param vari pelinappulan väri
+	 * @return ImageView olio kuvasta halutussa värissä
+	 */
 	public static ImageView getRatsu(NappulanVari vari) {
 		return new ImageView(new Image(kuvaURI + "hevonen" + getVariURI(vari)));
 	}
 
+	/**
+	 * Metodi joka palauttaa Torni -pelinappulan halutussa värissä
+	 * 
+	 * @param vari pelinappulan väri
+	 * @return ImageView olio kuvasta halutussa värissä
+	 */
 	public static ImageView getTorni(NappulanVari vari) {
 		return new ImageView(new Image(kuvaURI + "torni" + getVariURI(vari)));
 	}
 
+	/**
+	 * Metodi joka palauttaa Lähetti -pelinappulan halutussa värissä
+	 * 
+	 * @param vari pelinappulan väri
+	 * @return ImageView olio kuvasta halutussa värissä
+	 */
 	public static ImageView getLahetti(NappulanVari vari) {
 		return new ImageView(new Image(kuvaURI + "lahetti" + getVariURI(vari)));
 	}
 
+	/**
+	 * Metodi joka palauttaa Kuningatar -pelinappulan halutussa värissä
+	 * 
+	 * @param vari pelinappulan väri
+	 * @return ImageView olio kuvasta halutussa värissä
+	 */
 	public static ImageView getKuningatar(NappulanVari vari) {
 		return new ImageView(new Image(kuvaURI + "kuningatar" + getVariURI(vari)));
 	}
 
+	/**
+	 * Metodi joka palauttaa Kuningas -pelinappulan halutussa värissä
+	 * 
+	 * @param vari pelinappulan väri
+	 * @return ImageView olio kuvasta halutussa värissä
+	 */
 	public static ImageView getKuningas(NappulanVari vari) {
 		return new ImageView(new Image(kuvaURI + "kuningas" + getVariURI(vari)));
 	}
 
+	/**
+	 * Metodi joka palauttaa halutun string päätteen kuvien nimille toiston
+	 * välttämiseksi.
+	 * 
+	 * @param vari pelinappulan väri
+	 * @return Stringinä oikean päätteen kuvan nimelle
+	 */
 	private static String getVariURI(NappulanVari vari) {
 		if (vari == NappulanVari.VALKOINEN) {
 			return "_v.png";
